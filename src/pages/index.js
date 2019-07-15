5063aebe51f76589ecc0bad2d63108f4f8f1144a
ronald <ronaldlangeveld@gmail.com>
2019-07-15 11:40:47 +0800

    Added lazy loading background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,44 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import { StaticQuery, graphql } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Header from "../components/header"
 import Projects from "../components/projects"
+import BackgroundImage from 'gatsby-background-image'
 
 
 const IndexPage = () => (
+  <StaticQuery
+  query={graphql`
+  query {
+    desktop: file(relativePath: { eq: "bg.JPG" }) {
+      childImageSharp {
+        fluid(quality: 90, maxWidth: 4160) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`} render={data => (
   <Layout>
     <SEO title="Ronald Langeveld | Full Stack Web Developer and Bootstrapper" />
-    <header style={bgImg} className="masthead">
+    <BackgroundImage Tag="section"
+                           className={`masthead`}
+                           fluid={data.desktop.childImageSharp.fluid}
+                           backgroundColor={`#040e18`}>
+    {/* <header style={bgImg} className="masthead"> */}
     <div className="overlay"></div>
       <div className="overlay"></div>
       <div className="overlay"></div>
     <Header />
     <Projects />
-    </header>
-
-
-
-
+    {/* </header> */}
+    </BackgroundImage>
   </Layout>
+)}
+/>
 )
 const bgImg = {
   backgroundImage: "url(" + "'https://res.cloudinary.com/cinemakers/image/upload/v1563114654/other/IMG_0774.jpg'" + ")",
