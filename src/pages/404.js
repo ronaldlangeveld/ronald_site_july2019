0f3dd3438bf683c78d941fc02c368ab83278a4ca
ronald <ronaldlangeveld@gmail.com>
2019-07-22 10:19:12 +0800

    added custom 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,24 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section class="hero is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title is-1 has-text-weight-light">
+        404 🥺
+      </h1>
+      <h2 class="subtitle">
+      Sorry, something went wrong and this page cannot be found. 
+      </h2>
+   <Link to="/">Go to homepage</Link>
+    </div>
+  </div>
+</section>
   </Layout>
 )
 
