688da41c4808e75bf67702c921655cbe4f959cbb
ronald <ronaldlangeveld@gmail.com>
2019-07-30 11:59:01 +0800

    latest and greatest

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Img from 'gatsby-image';
 import { StaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
-import BackgroundImage from 'gatsby-background-image'
+
 
 export default () => (
   <StaticQuery
@@ -20,69 +20,61 @@ export default () => (
     render={data => (
       <div>
 
-        <section className="hero is-fullheight">
+        <section className="hero">
+          <div className="hero-header">
+            <div className="container">
+              <div className="columns is-centered">
+                <div className="column is-6">
+                  <br />
+                  <h1 className="title has-text-weight-light has-text-centered">Hi, I'm Ronald</h1>
 
+
+
+
+                  <hr />
+
+
+                  <nav class="level is-mobile">
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <Link to="/blog" className="has-text-dark">Blog</Link>
+                      </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <Link to='/projects' className="has-text-dark">Projects</Link>
+                      </div>
+                    </div>
+
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <Link to='/contact' className="has-text-dark" >Around the web</Link>
+                      </div>
+                    </div>
+                    <div class="level-item has-text-centered">
+                      <div>
+                        <a href="https://twitter.com/ronaldlangeveld" target="_blank" className="has-text-dark">Twitter</a>
+                      </div>
+                    </div>
+                  </nav>
+
+                </div>
+              </div>
+            </div>
+          </div>
           <div className="hero-body">
             <div className="container">
-              <div className="columns">
+              <div className="columns is-centered">
                 <div className="column is-6">
-                  <div className="borderLeft">
-                    <p className="is-size-4 has-text-weight-light ">Hi,</p>
-                    <h1 className="title is-3 has-text-weight-light ">
-                      I'm Ronald
-          </h1>
+                <Img fluid={data.main.childImageSharp.fluid} />
+                <br />
+                  <div className="content">
+                    <p>I’m a freelance software engineer, with 3 years experience using <strong>Python 3</strong> and the <strong>Django web framework</strong>. I keep my applications running smoothly and automate tasks by deploying them onto <strong>Linux</strong> servers.</p>
+                    <p>More recently I’m developing web applications with <strong>NodeJS</strong> and <strong>ReactJS</strong> while learning <strong>React Native</strong> to build iOS and.</p>
+                    <p><strong>Need Web Development</strong> or <strong>Tech Consultation</strong> to kick off for your next big thing? <br /> <Link to='/contact'>Let’s get in touch.</Link></p>
 
-                    <nav className="level">
 
-                      <div className="level-item level-left">
-                        <div>
-                          <h2 className="subtitle is-4 has-text-weight-light ">
-                            a full stack web developer
-          </h2>
-                        </div>
-                      </div>
-
-                      <div className="level-item level-right">
-                        <div>
-                          <a className="has-text-black" href="https://twitter.com/ronaldlangeveld" target="_blank">Twitter</a> <a className="has-text-black" style={littleRight} href="mailto:hi@ronaldlangeveld.com">Email</a>
-
-                        </div>
-                      </div>
-                    </nav>
-
-                    <hr className="hr_line" />
-
-                    <p className="is-size-5 has-text-weight-light ">
-                      Need Web Development or Tech Consultation to kick off for your next big thing? <u><Link to="/contact" className="has-text-dark">Let's get in touch.</Link></u>
-          </p>
-
-          <br />
-
-                    <p className="is-size-6 has-text-weight-light ">
-                 
-                       <u><Link to="/projects" className="is-size-6 has-text-dark">See some of my work</Link> </u>
-          </p>
-                    <br />
-
-                    <div>
-                    <u><Link to="/blog" className="is-size-6 has-text-dark has-text-weight-light">Read my blog</Link> </u>
-
-                      <br />
-                      <br />
-                      <br />
-
-                    </div>
                   </div>
-                </div>
-
-                <div className="column is-hidden-touch">
-                  <BackgroundImage Tag="section"
-                    className={`masthead`}
-                    fluid={data.main.childImageSharp.fluid}
-                    backgroundColor={`#040e18`} />
-
-                  <p className="help has-text-right">Me in Kuala Lumpur, July 2019</p>
-
                 </div>
 
               </div>
